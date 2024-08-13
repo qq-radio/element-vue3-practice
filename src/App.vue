2578fac89fd5bfdeba03e8799793e895dd036ac4
qq-radio <2433378670@qq.com>
2024-08-13 20:44:23 +0800

    大家都是可怜的人间

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,8 @@
     <BaseHeader />
     <div class="flex main-container">
       <BaseSide />
-      <div w="full" py="4">
-        <Logos my="4" />
-        <HelloWorld msg="Hello Vue 3 + Element Plus + Vite" />
+      <div w="full" m="1.5" bg-white>
+        <BasicTable msg="Hello Vue 3 + Element Plus + Vite" />
       </div>
     </div>
   </el-config-provider>
@@ -15,9 +14,23 @@
 #app {
   text-align: center;
   color: var(--ep-text-color-primary);
+  background: #f0f2f5;
 }
 
 .main-container {
   height: calc(100vh - var(--ep-menu-item-height) - 3px);
+}
+
+::-webkit-scrollbar-track-piece {
+  background-color: transparent;
+}
+::-webkit-scrollbar {
+  width: 7px;
+  height: 7px;
+  background-color: transparent;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 5px;
+  background-color: hsla(220, 4%, 58%, 0.3);
 }
 </style>
