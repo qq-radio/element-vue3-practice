84423e33d79d3673a8372deec240f4c67faeec57
qq-radio <2433378670@qq.com>
2024-09-02 08:28:51 +0800

    不要做超出自己能力太多的事情

--- src/components/basic-table/src/components/TableBody.vue
+++ src/components/basic-table/src/components/TableBody.vue
@@ -1,16 +1,14 @@
 <template>
-  <div ref="tableWrapperInstance" class="plus-table">
+  <div class="plus-table">
     <el-table
-      v-bind="$attrs"
       v-loading="loading"
+      v-bind="$attrs"
       :data="tableDatas"
-      :border="true"
-      :size="size"
       :row-key="rowKey"
+      :border="true"
       highlight-current-row
       scrollbar-always-on
     >
-      <!-- 默认插槽 -->
       <template #default>
         <slot name="default">
           <!-- 选择栏 -->
@@ -97,12 +95,10 @@
         </slot>
       </template>
 
-      <!-- 插入至表格最后一行之后的内容 -->
       <template #append>
         <slot name="append" />
       </template>
 
-      <!-- 当数据为空时自定义的内容 -->
       <template #empty>
         <slot name="empty" />
       </template>
@@ -121,23 +117,23 @@ import {
   useSlots,
   unref,
   computed,
-} from "vue";
-import type { PlusPaginationProps } from "@plus-pro-components/components/pagination";
+} from 'vue'
+import type { PlusPaginationProps } from '@plus-pro-components/components/pagination'
 import {
   DefaultPageInfo,
   TableFormRefInjectionKey,
   TableFormFieldRefInjectionKey,
-} from "@plus-pro-components/constants";
-import type { Ref, ComputedRef } from "vue";
-import type { ComponentSize } from "element-plus/es/constants";
-import type { TableInstance } from "element-plus";
-import { ElTable, ElTableColumn, vLoading } from "element-plus";
+} from '@plus-pro-components/constants'
+import type { Ref, ComputedRef } from 'vue'
+import type { ComponentSize } from 'element-plus/es/constants'
+import type { TableInstance } from 'element-plus'
+import { ElTable, ElTableColumn, vLoading } from 'element-plus'
 import type {
   PageInfo,
   PlusColumn,
   RecordType,
   FormFieldRefsType,
-} from "@plus-pro-components/types";
+} from '@plus-pro-components/types'
 import {
   getTableCellSlotName,
   getTableHeaderSlotName,
@@ -145,11 +141,11 @@ import {
   getExtraSlotName,
   filterSlots,
   isSVGElement,
-} from "@plus-pro-components/components/utils";
-import { cloneDeep } from "lodash-es";
-import PlusTableActionBar from "./table-action-bar.vue";
-import PlusTableColumn from "./table-column.vue";
-import PlusTableTableColumnIndex from "./table-column-index.vue";
+} from '@plus-pro-components/components/utils'
+import { cloneDeep } from 'lodash-es'
+import PlusTableActionBar from './table-action-bar.vue'
+import PlusTableColumn from './table-column.vue'
+import PlusTableTableColumnIndex from './table-column-index.vue'
 import type {
   ButtonsCallBackParams,
   PlusTableState,
@@ -157,15 +153,15 @@ import type {
   PlusTableEmits,
   TableFormRefRow,
   FormChangeCallBackParams,
-} from "./type";
+} from './type'
 
 defineOptions({
-  name: "PlusTable",
+  name: 'PlusTable',
   inheritAttrs: false,
-});
+})
 
 const props = withDefaults(defineProps<PlusTableProps>(), {
-  defaultSize: "default",
+  defaultSize: 'default',
   pagination: false,
   actionBar: false,
   hasIndexColumn: false,
@@ -176,7 +172,7 @@ const props = withDefaults(defineProps<PlusTableProps>(), {
   data: () => [],
   columns: () => [],
 
-  rowKey: "id",
+  rowKey: 'id',
   indexTableColumnProps: () => ({}),
   indexContentStyle: () => ({}),
   selectionTableColumnProps: () => ({
@@ -184,184 +180,85 @@ const props = withDefaults(defineProps<PlusTableProps>(), {
   }),
   expandTableColumnProps: () => ({}),
   editable: false,
-});
-const emit = defineEmits<PlusTableEmits>();
+})
+const emit = defineEmits<PlusTableEmits>()
 
-const subColumns: Ref<PlusColumn[]> = ref([]);
-const columnsIsChange: Ref<boolean> = ref(false);
-const filterColumns: Ref<PlusColumn[]> = ref([]);
-const tableWrapperInstance = ref<HTMLDivElement | null>(null);
-const state = reactive<PlusTableState>({
-  subPageInfo: {
-    ...(((props.pagination as PlusPaginationProps)?.modelValue ||
-      DefaultPageInfo) as PageInfo),
-  },
-  size: props.defaultSize,
-});
+const subColumns: Ref<PlusColumn[]> = ref([])
+const columnsIsChange: Ref<boolean> = ref(false)
+const filterColumns: Ref<PlusColumn[]> = ref([])
 
-const tableDatas = computed<RecordType[]>(() => props.data);
+const tableDatas = computed<RecordType[]>(() => props.data)
 
-const slots = useSlots();
+const slots = useSlots()
 
 /**
  * 表格单元格的插槽
  */
-const cellSlots = filterSlots(slots, getTableCellSlotName());
+const cellSlots = filterSlots(slots, getTableCellSlotName())
 
 /**
  * 表格单元格表头的插槽
  */
-const headerSlots = filterSlots(slots, getTableHeaderSlotName());
+const headerSlots = filterSlots(slots, getTableHeaderSlotName())
 
 /**
  * 表单单项的插槽
  */
-const fieldSlots = filterSlots(slots, getFieldSlotName());
+const fieldSlots = filterSlots(slots, getFieldSlotName())
 
 /**
  * el-form-item 下一行额外的内容 的插槽
  */
-const extraSlots = filterSlots(slots, getExtraSlotName());
+const extraSlots = filterSlots(slots, getExtraSlotName())
 
 /**
  * 表单的ref
  */
-const formRefs = shallowRef<Record<string | number, TableFormRefRow[]>>({});
-provide(TableFormRefInjectionKey, formRefs);
+const formRefs = shallowRef<Record<string | number, TableFormRefRow[]>>({})
+provide(TableFormRefInjectionKey, formRefs)
 /**
  * 表单Field的ref
  */
-const formFieldRefs = shallowRef<FormFieldRefsType>({});
-provide(TableFormFieldRefInjectionKey, formFieldRefs);
+const formFieldRefs = shallowRef<FormFieldRefsType>({})
+provide(TableFormFieldRefInjectionKey, formFieldRefs)
 
 // 监听配置更改
 watch(
   () => props.columns,
   (val) => {
-    subColumns.value = val.filter((item) => unref(item.hideInTable) !== true);
-    filterColumns.value = cloneDeep(subColumns.value);
-    columnsIsChange.value = !columnsIsChange.value;
+    subColumns.value = val.filter((item) => unref(item.hideInTable) !== true)
+    filterColumns.value = cloneDeep(subColumns.value)
+    columnsIsChange.value = !columnsIsChange.value
   },
   {
     deep: true,
     immediate: true,
   }
-);
-
-// 发分页改变事件
-const handlePaginationChange = () => {
-  emit("paginationChange", { ...state.subPageInfo });
-};
+)
 
 const handleAction = (callbackParams: ButtonsCallBackParams) => {
-  emit("clickAction", callbackParams);
-};
+  emit('clickAction', callbackParams)
+}
 
 const handleClickActionConfirmCancel = (
   callbackParams: ButtonsCallBackParams
 ) => {
-  emit("clickActionConfirmCancel", callbackParams);
-};
+  emit('clickActionConfirmCancel', callbackParams)
+}
 
 const handleFilterTableConfirm = (_columns: PlusColumn[]) => {
-  filterColumns.value = _columns;
+  filterColumns.value = _columns
   subColumns.value = _columns.filter(
     (item) =>
       unref(item.hideInTable) !== true && item.__selfHideInTable !== true
-  );
-};
+  )
+}
 
 const handleFormChange = (data: FormChangeCallBackParams) => {
-  emit("formChange", data);
-};
-
-// 保存活动的表单
-const currentForm = ref();
-
-const handleCellEdit = (
-  row: RecordType,
-  column: PlusColumn,
-  type: "click" | "dblclick"
-) => {
-  const rowIndex = tableDatas.value.indexOf(row);
-  const columnIndex = column.index;
-  const columnConfig = subColumns.value[column.index];
-
-  // 不是可编辑行，如操作栏
-  if (!columnConfig) return;
-
-  if (props.editable === type) {
-    document.addEventListener("click", handleStopEditClick);
-
-    const currentCellForm = formRefs.value[rowIndex][columnIndex];
-
-    // 停止上一个表单的编辑状态
-    if (currentForm.value) {
-      currentForm.value?.stopCellEdit();
-    }
-    currentForm.value = currentCellForm;
-
-    // 开启当前点击的单元格的编辑
-    currentCellForm.startCellEdit();
-
-    // 当表单初始化完成
-    const unwatch = watch(
-      () => formFieldRefs.value.valueIsReady,
-      (val) => {
-        if (
-          val?.value &&
-          formFieldRefs.value?.fieldInstance?.focus &&
-          (props.editable === "click" || props.editable === "dblclick")
-        ) {
-          formFieldRefs.value.fieldInstance.focus();
-          // 销毁监听
-          unwatch();
-        }
-      }
-    );
-  }
-};
-
-const handleClickCell = (
-  row: RecordType,
-  column: PlusColumn,
-  cell: HTMLTableCellElement,
-  event: Event
-) => {
-  handleCellEdit(row, column, "click");
-  emit("cell-click", row, column, cell, event);
-};
-
-const handleDoubleClickCell = (
-  row: RecordType,
-  column: PlusColumn,
-  cell: HTMLTableCellElement,
-  event: Event
-) => {
-  handleCellEdit(row, column, "dblclick");
-  emit("cell-dblclick", row, column, cell, event);
-};
-
-// 退出编辑状态
-const handleStopEditClick = (e: MouseEvent) => {
-  if (tableWrapperInstance.value && currentForm.value) {
-    const wrapperClass = ".el-table__body-wrapper";
-    const tbody = tableWrapperInstance.value.querySelector(wrapperClass)!;
-    const target = e?.target as HTMLElement;
-    const cls = Array.from(target.classList).join(".");
-    const tempCls = cls ? `.${cls}` : "";
-    const contains = tempCls && tbody.querySelector(tempCls);
-    if (!contains && !isSVGElement(target)) {
-      currentForm.value?.stopCellEdit();
-      emit("edited");
-      document.removeEventListener("click", handleStopEditClick);
-    }
-  }
-};
-
-const { subPageInfo, size } = toRefs(state);
+  emit('formChange', data)
+}
 
 defineExpose({
   formRefs,
-});
+})
 </script>
--- src/components/basic-table/src/types/index.ts
+++ src/components/basic-table/src/types/index.ts
@@ -1,60 +1,63 @@
-import type { TableProps, PaginationProps } from "element-plus";
+import type { TableProps, PaginationProps } from 'element-plus'
 
 import type {
   BasicFormProps,
   FormSchema,
-} from "@/components/basic-form/src/types";
-import type { Page } from "@/components/basic-pagination";
+} from '@/components/basic-form/src/types'
+import type { Page } from '@/components/basic-pagination'
 
 export type PageMap = {
-  [K in keyof Pick<Page, "pageSize" | "currentPage">]: string;
-};
+  [K in keyof Pick<Page, 'pageSize' | 'currentPage'>]: string
+}
 
 export interface BasicTableProps {
-  schemas: TableSchema[];
-  tableDatas?: Array<Recordable>;
+  schemas: TableSchema[]
+  tableDatas?: Array<Recordable>
   request?: (params: Recordable) => Promise<{
     data: {
-      records: Array<Recordable>;
-      total: number;
-    };
-  }>;
-  pageMap?: PageMap;
-  extraParams?: Recordable;
-  paramsFormatter?: (params: Recordable) => Recordable;
-  responseFormatter?: (tableDatas: Array<Recordable>) => Array<Recordable>;
-  showIndexColumn?: boolean;
-  immediate?: boolean;
-  loading?: boolean;
-  ellipsis?: boolean;
+      records: Array<Recordable>
+      total: number
+    }
+  }>
+  pageMap?: PageMap
+  extraParams?: Recordable
+  paramsFormatter?: (params: Recordable) => Recordable
+  responseFormatter?: (tableDatas: Array<Recordable>) => Array<Recordable>
+  showIndexColumn?: boolean
+  immediate?: boolean
+  loading?: boolean
+  ellipsis?: boolean
 
-  searchProps?: Partial<BasicFormProps>;
-  tableProps?: Partial<TableProps<unknown>>;
-  paginationProps?: Partial<PaginationProps>;
+  searchProps?: Partial<BasicFormProps>
+  tableProps?: Partial<TableProps<unknown>>
+  paginationProps?: Partial<PaginationProps>
 }
 
 export interface BasicTableEmits {
-  (e: "search", params: Recordable): void;
-  (e: "reset", params: Recordable): void;
-  (e: "pagination-change", page: Page): void;
-  (e: "request-error", error: unknown): void;
-  (e: "request-complete", tableDatas: Array<Recordable>): void;
+  (e: 'search', params: Recordable): void
+  (e: 'reset', params: Recordable): void
+  (e: 'pagination-change', page: Page): void
+  (e: 'request-error', error: unknown): void
+  (e: 'request-complete', tableDatas: Array<Recordable>): void
 }
 
-export type RenderType = "img" | "link" | "tag" | "progress" | "copy";
+export type RenderType = 'img' | 'link' | 'tag' | 'progress' | 'copy'
 
-export type CustomRenderType = string | VNode | JSX.Element | Component;
+export type CustomRenderType = string | VNode | JSX.Element | Component
 
 export interface TableSchema {
-  label: string;
-  prop: string;
-  visible?: boolean;
-  width?: string | number;
-  renderType?: RenderType;
+  label: string
+  prop: string
+  visible?: boolean
+  width?: string | number
+  renderType?: RenderType
   customRender?: (
     value: any,
     data: { row: Recordable; column: TableSchema; index: number }
-  ) => CustomRenderType;
-  searchable?: boolean;
-  searchConfig?: FormSchema;
+  ) => CustomRenderType
+  searchable?: boolean
+  searchConfig?: FormSchema
 }
+
+// 它的设计原则是，render>slot>valueType 所有为什么这样？
+// 我最好应该把renderType改为displayType，这样我才好解释上面这句话
