945c89bf23c886256446a669fa4ee1fc7a871159
qq-radio <2433378670@qq.com>
2024-09-03 08:23:13 +0800

    dont let me down

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Schema Driven Development',
+  description: 'A VitePress Site',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples??',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+      {
+        text: 'Why??',
+        items: [{ text: 'whyme', link: '/whyme' }],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+})
